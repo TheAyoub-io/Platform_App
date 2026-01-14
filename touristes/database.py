170ac8f86d1562f298d79343d9bbf5b76f6cf17c
Ahmed-KM-sudo <kassmimohamed93@gmail.com>
2026-01-14 17:12:07 +0100

    refactor: Remove Flask files and dependencies

--- touristes/database.py
+++ touristes/database.py
@@ -14,7 +14,6 @@ Base = declarative_base()
 
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
-from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(Base):
     __tablename__ = "users"
@@ -24,11 +23,6 @@ class User(Base):
     email = Column(String, unique=True, index=True, nullable=False)
     password_hash = Column(String, nullable=False)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
 
 class Destination(Base):
     __tablename__ = "destination"
